22dd6116ef2521e4ebbaee35f2f71523cfbab987
JVBALANE <balanejohnvincent16@gmail.com>
2025-10-27 20:55:08 +0800

    Add user login functionality in activity9.py

---
+++ activity9.py
@@ -0,0 +1,14 @@
+a = 3
+b = 2
+
+c = 6
+d = 7
+
+username = 'Jv'
+passsword = 'bulog3'
+
+print("USER LOGIN ")
+print( (username == 'Jv') and (passsword == 'bulog3'))
+
+#print( (a > b) and (c < d) )
+#print( (a < b) or (c < d) )
