85de6cfb3db5f379d32b11227750d0783bc44dbd
JVBALANE <balanejohnvincent16@gmail.com>
2025-10-27 21:14:23 +0800

    Add greeting message with user input

---
+++ code_challenge1.py
@@ -0,0 +1,2 @@
+name = input("Type your name --> ")
+print("\t\t\t\t\t*\n\t\t\t\t*\t\t*\n\t\t\t*", "\tHi", "\t\t\t*\n\t\t*\t\t\t", "JV", "\t\t\t*\n\t*\t\t\t\t\t\t\t\t*\n\t\t*\t\t\t\t\t\t*\n\t\t\t*\t\t\t\t*\n\t\t\t\t*\t\t*\n\t\t\t\t\t*")
