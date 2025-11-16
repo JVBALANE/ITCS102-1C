5e44cf4c95f559d949a37656bfee86bd0f71d401
JVBALANE <balanejohnvincent16@gmail.com>
2025-11-16 23:31:17 +0800

    Add MyFirstFunction and greeting functionalities

---
+++ activity23.py
@@ -0,0 +1,11 @@
+from activity23_1 import GreetWithName, GreetPerson, FunctionWithReturn, Factorial
+#Defining a function
+def MyFirstFunction():
+    print("Hi!! Pogi")
+    print("How are u? ")
+
+MyFirstFunction()   
+GreetWithName('Jv')
+GreetPerson('Vincent' 'Lucena' '18')
+print(f"I want to get the summation of {FunctionWithReturn(7)}")
+print(f"The factorial of {Factorial(6)}")
