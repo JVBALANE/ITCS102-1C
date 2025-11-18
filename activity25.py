1a0d6ce2b61e433ef8d4f17a3c278aa8456f14b5
JVBALANE <balanejohnvincent16@gmail.com>
2025-11-18 21:59:42 +0800

    Add list and dictionary of programming languages
    Define a list of programming languages and a dictionary mapping names to languages.

---
+++ activity25.py
@@ -0,0 +1,16 @@
+#LIST
+
+programming_language = ['java', 'C#', 'C++', 'C', 'Perl', 'Python', 'JavaScript', 'Rust', 'GoLang']
+
+print(programming_language)
+
+#DICTIONARY
+
+emtpy_dictionary = {} #empty dictionary
+# { KEY  VALUE } : dictionary has no index, it uses 'KEYS'
+
+pl_dictionary = {'lance' : 'java', 'ash' : 'C#', 'angkol' : 'C++',\
+                  'rins' : 'C', 'rix' : 'Perl', 'tames' : 'Python',\
+                      'kisha' : 'JavaScript', 'angelo' : 'Rust' 'yani' : 'GoLang''}
+
+print(pl_dictionary['telly'])                         
